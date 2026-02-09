e47a7ba5ae4e3382f26dbdc38ed4db8995c3bdfe
Criton08 <cri.delledonne08@gmail.com>
2026-02-09 20:58:10 +0100

    Aggiunta comandi mappa, player nella libreria

--- PoopMan/Game1.cs
+++ PoopMan/Game1.cs
@@ -2,6 +2,8 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using PoopManLibrary;
+using PoopManLibrary.World;
+using PoopManLibrary.Entities;
 
 namespace PoopMan
 {
@@ -10,29 +12,43 @@ namespace PoopMan
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        public Game1() : base("PoopMan", 1280, 720, false)
+        private TileMap map;
+        private Player player;
+        private Texture2D pixel;
+
+        public Game1() : base("PoopMan", 416, 480, false)
         {
 
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
+            // TODO: Add your initialization logic here
         }
 
         protected override void LoadContent()
         {
             // TODO: use this.Content to load your game content here
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            // Pixel per test
+            pixel = new Texture2D(GraphicsDevice, 1, 1);
+            pixel.SetData(new[] { Color.White });
+
+            //creo la mappa
+            map = new TileMap(pixel);
+
+            //creo il player
+            player = new Player(pixel, new Point(1, 1));
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
+            player.Update(map, Keyboard.GetState());
 
             base.Update(gameTime);
         }
@@ -42,6 +58,12 @@ namespace PoopMan
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            map.Draw(_spriteBatch);
+            _spriteBatch.Draw(player.Texture, new Rectangle((int)player.Position.X, (int)player.Position.Y, TileMap.TileSize, TileMap.TileSize), Color.White);
+            
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
---
+++ PoopManLibrary/Entities/Player.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using PoopManLibrary.World; // per TileMap e TileType
+
+namespace PoopManLibrary.Entities
+{
+    public class Player
+    {
+        public Point TilePosition; // Posizione del giocatore sulla griglia
+        public Texture2D Texture; // Texture del giocatore
+        public Vector2 Position; // Posizione del giocatore in pixel
+        public int Speed = 2; // Velocità di movimento del giocatore 
+        
+        //costruttore
+        public Player(Texture2D texture,Point startTile)
+        {
+            texture = Texture;
+            TilePosition = startTile;
+            Position = new Vector2(TilePosition.X * TileMap.TileSize, TilePosition.Y * TileMap.TileSize);
+            //setto la pos iniziale del player dimensionandolo alla tile
+        }
+        public void Update(TileMap map,KeyboardState keyboardstate)
+        {
+            Point targetTile = TilePosition; // tile di destinazione inizialmente uguale alla tile attuale
+
+            // Controllo input per il movimento
+            if (keyboardstate.IsKeyDown(Keys.W)) targetTile.Y -= 1; // su
+            else if (keyboardstate.IsKeyDown(Keys.S)) targetTile.Y += 1; // giù
+            else if (keyboardstate.IsKeyDown(Keys.A)) targetTile.X -= 1; // sinistra
+            else if (keyboardstate.IsKeyDown(Keys.D)) targetTile.X += 1; // destra
+
+            if(map.IsWalkable(targetTile)) // se la tile di destinazione è camminabile
+            {
+                TilePosition = targetTile; // aggiorno la posizione del giocatore
+                Position = new Vector2(TilePosition.X * TileMap.TileSize, TilePosition.Y * TileMap.TileSize); // aggiorno la posizione in pixel
+            }
+        }
+    }
+}
